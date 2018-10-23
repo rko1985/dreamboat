153fed0def11a2a2c6925cf258c3da472c6b516b
rko1985 <35703264+rko1985@users.noreply.github.com>
2018-10-23 11:17:05 -1000

    basic search functions working

--- boat_search.php
+++ boat_search.php
@@ -11,9 +11,88 @@
         <input type="text" class="form-control" name="year_beg" placeholder="ex. 1965">
         <input type="text" class="form-control" name="year_end" placeholder="ex. 2015">
     </div>
+
+    <div class="form-group">
+        <label for="boat_type">Boat Type</label>
+        <select name="boat_type" class="form-control">
+            <option value=''>Any</option>
+            <option value="1">Sail</option>
+            <option value="2">Power</option> 
+            <option value="3">Motor Sail</option> 
+            <option value="4">Fishing</option>     
+        </select>
+    </div>
+
+    <div class="form-group">
+        <label for="builder">Builder</label><br>
+        <select name="builder" id="builder" class="form-control">
+            <option value=''>Any</option>
+            <option value="Ranger">Ranger</option>
+            <option value="Coronado">Coronado</option> 
+            <option value="Rhoades">Rhoades</option>                           
+        </select>
+    </div>
+
+    <div class="form-group">
+        <label for="designer">Designer</label><br>
+        <select name="designer" id="designer" class="form-control">
+            <option value=''>Any</option>
+            <option value="gary_mull">Gary Mull</option>
+            <option value="ed_edgar">Ed Edgar</option> 
+            <option value="frank_butler">Frank Butler</option>                           
+        </select>
+    </div>
+
+    <div class="form-group">
+        <label for="loa">LOA</label>
+        <input type="text" class="form-control" name="loa_min" placeholder="ex. 1">
+        <input type="text" class="form-control" name="loa_max" placeholder="ex. 5000">
+    </div>
+
+    <div class="form-group">
+        <label for="lod">LOD</label>
+        <input type="text" class="form-control" name="lod_min" placeholder="ex. 1">
+        <input type="text" class="form-control" name="lod_max" placeholder="ex. 5000">
+    </div>
+
+    <div class="form-group">
+        <label for="LWL">LWL</label>
+        <input type="text" class="form-control" name="lwl_min" placeholder="ex. 1">
+        <input type="text" class="form-control" name="lwl_max" placeholder="ex. 5000">
+    </div>
+
+    <div class="form-group">
+        <label for="beam">BEAM</label>
+        <input type="text" class="form-control" name="beam_min" placeholder="ex. 1">
+        <input type="text" class="form-control" name="beam_max" placeholder="ex. 5000">
+    </div>
+
+    <div class="form-group">
+        <label for="ballast">Ballast</label>
+        <input type="text" class="form-control" name="ballast_min" placeholder="ex. 1">
+        <input type="text" class="form-control" name="ballast_max" placeholder="ex. 5000">
+    </div>
+
+    <div class="form-group">
+        <label for="displacement">Displacement</label>
+        <input type="text" class="form-control" name="displacement_min" placeholder="ex. 1">
+        <input type="text" class="form-control" name="displacement_max" placeholder="ex. 5000">
+    </div>
+
+    <div class="form-group">
+        <label for="ballast_displacement">Ballast/Displacement</label>
+        <input type="text" class="form-control" name="ballast_displacement_min" placeholder="ex. 1">
+        <input type="text" class="form-control" name="ballast_displacement_max" placeholder="ex. 5000">
+    </div>
+
+    <div class="form-group">
+        <label for="draft">Draft</label>
+        <input type="text" class="form-control" name="draft_min" placeholder="ex. 1">
+        <input type="text" class="form-control" name="draft_max" placeholder="ex. 5000">
+    </div>
     
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="Search_Boat" value="Search For Boat">
+        <input class="btn btn-primary float-right" type="submit" name="Search_Boat" value="Search For Boat">
     </div>
 </form>
 
--- boat_search_results.php
+++ boat_search_results.php
@@ -18,26 +18,83 @@
 
             $year_beg = $_GET['year_beg'];
             $year_end = $_GET['year_end'];
+            $loa_min = $_GET['loa_min'];
+            $loa_max = $_GET['loa_max'];
+            $boat_type = $_GET['boat_type'];
+            $builder = $_GET['builder'];
+            $designer = $_GET['designer'];
+            $lod_min = $_GET['lod_min'];
+            $lod_max = $_GET['lod_max'];
+            $lwl_min = $_GET['lwl_min'];
+            $lwl_max = $_GET['lwl_max'];
+            $beam_min = $_GET['beam_min'];
+            $beam_max = $_GET['beam_max'];
+            $ballast_min = $_GET['ballast_min'];
+            $ballast_max = $_GET['ballast_max'];
+            $displacement_min = $_GET['displacement_min'];
+            $displacement_max = $_GET['displacement_max'];
+            $ballast_displacement_min = $_GET['ballast_displacement_min'];
+            $ballast_displacement_max = $_GET['ballast_displacement_max'];
+            $draft_min = $_GET['draft_min'];
+            $draft_max = $_GET['draft_max'];
 
-            $query = "SELECT * FROM boats WHERE boat_year BETWEEN $year_beg AND $year_end";
+        
+            $query = "SELECT * FROM boats WHERE ";
+            if($year_beg && $year_end !== null) {
+                $query .= "(boat_year BETWEEN $year_beg AND $year_end) ";
+            }
+            if($loa_min && $loa_max !== null) {
+                $query .= "(LOA BETWEEN $loa_min AND $loa_max) ";
+            }
+            if(!empty($boat_type)) {
+                $query .= "(boat_type = $boat_type) ";
+            }
+            if(!empty($builder)) {
+                $query .= "(builder = '{$builder}' ) ";
+            }
+            if(!empty($designer)) {
+                $query .= "(designer = '{$designer}') ";
+            }
+            if($lod_min && $lod_max !== null) {
+                $query .= "(LOD BETWEEN $lod_min AND $lod_max) ";
+            }
+            if($lwl_min && $lwl_max !== null) {
+                $query .= "(LWL BETWEEN $lwl_min AND $lwl_max) ";
+            }
+            if($beam_min && $beam_max !== null) {
+                $query .= "(beam BETWEEN $beam_min AND $beam_max) ";
+            }
+            if($ballast_min && $ballast_max !== null) {
+                $query .= "(ballast BETWEEN $ballast_min AND $ballast_max) ";
+            }
+            if($displacement_min && $displacement_max !== null) {
+                $query .= "(displacement BETWEEN $displacement_min AND $displacement_max) ";
+            }
+            if($ballast_displacement_min && $ballast_displacement_max !== null) {
+                $query .= "(ballast_displacement BETWEEN $ballast_displacement_min AND $ballast_displacement_max) ";
+            }
+            if($draft_min && $draft_max !== null) {
+                $query .= "(draft BETWEEN $draft_min AND $draft_max) ";
+            }
+                
+
+        
+            
             $boat_search_query = mysqli_query($connection, $query);
 
             if(!$boat_search_query){
                 echo mysqli_error($connection);
             }
 
-        
-        
-            $query = "SELECT * FROM boats WHERE boat_year BETWEEN $year_beg AND $year_end";
-            $select_boats= mysqli_query($connection, $query);
+                        
 
-            while($row = mysqli_fetch_assoc($select_boats)) {
+            while($row = mysqli_fetch_assoc($boat_search_query)) {
                 $boat_id = $row['boat_id'];
                 $boat_name = $row['boat_name'];
                 $boat_year = $row['boat_year'];
                 $boat_type = $row['boat_type'];
                 $boat_image = $row['boat_image'];
-                
+                                
                 echo "<tr>";
                 echo "<td>$boat_id</td>";
                 echo "<td>$boat_name</td>";
@@ -54,12 +111,14 @@
 
                 }
 
-                echo "<td><img width='100' src='images/$boat_image' alt='image'></td>";            
+                echo "<td><img width='50' src='images/$boat_image' alt='image'></td>";            
                                             
                 
                                             
             }
-        
+
+            
+
         }
         ?>
    
